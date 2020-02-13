2e4f02b0cfb2c7017e5882f942e949e24d82f26d
Mike Schwartz <mikeschwartz@quickenloans.com>
2020-02-13 14:04:06 -0500

    Missed Human File

---
+++ Human.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab5_2
+{
+    class Human : Player
+    {
+        public Human(string name)
+            : base(name)
+        {
+            //GenerateRoshambo();
+        }
+
+        public override void GenerateRoshambo()
+        {
+            Console.Write("Please enter Rock, Paper, Or Scissors: ");
+            rpsChoice = Console.ReadLine().ToLower();
+
+        }
+
+        public override string ToString()
+        {
+            return $"{rpsChoice}";
+        }
+
+    }
+}
